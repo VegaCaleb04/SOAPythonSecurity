627cba8dbf8a45811abcece99b4cd097749f4509
vega <calebvega2001@gmail.com>
2023-08-25 08:04:58 -0500

    arreglado

--- Application/SOAPythonSecurity.py
+++ Application/SOAPythonSecurity.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
-import Core.Security.Encode.Base64Encode
-import Core.Security.Decode.Base64Decode
+from Core.Security.Encode import Base64Encode
+from Core.Security.Decode import Base64Decode
 
 app: FastAPI= FastAPI(title='SOAP python Security', description='USBSI 2023')
 
 app.get("/base64encode")
 async def base64_encode(plainText: str | None= None):
-    base64_encode = base64_encode()
+    base64_encode = Base64Encode()
     return base64_encode.encode(plainText)
 
 app.post("/base64Decode")
 async def base64_Decode(plainText: str | None= None):
-    base64_Decode = base64_Decode()
+    base64_Decode = Base64Decode()
     return base64_Decode.decode(plainText)
--- Core/Security/Decode/Base64Decode.py
+++ Core/Security/Decode/Base64Decode.py
@@ -1,6 +1,6 @@
 import base64
 
-class Base64Decode:
-     def decode(self, encoded_text):
-            decoded_bytes = base64.b64decode(encoded_text.encode('utf-8'))
-            return decoded_bytes.decode('utf-8')
+class Base64Encode:
+    def encode(self, plaintext):
+        encoded_bytes = base64.b64encode(plaintext.encode('utf-8'))
+        return encoded_bytes.decode('utf-8')
